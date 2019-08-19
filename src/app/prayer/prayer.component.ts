f5540b851830d2dc4cb152134765de2a69bdf3b5
Justin Shannon <42521970+papa7wheelie@users.noreply.github.com>
2019-08-18 21:23:03 -0400

    initial upload

---
+++ src/app/prayer/prayer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prayer',
+  templateUrl: './prayer.component.html',
+  styleUrls: ['./prayer.component.css']
+})
+export class PrayerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
