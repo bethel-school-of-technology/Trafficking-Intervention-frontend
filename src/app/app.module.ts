aed4b7f866da1d6a260ab76e9666f03e58b57eae
Justin Shannon <42521970+papa7wheelie@users.noreply.github.com>
2019-08-18 21:20:37 -0400

    modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { LocationsComponent } from './locations/locations.component';
+import { TestimoniesComponent } from './testimonies/testimonies.component';
+import { PrayerComponent } from './prayer/prayer.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    LocationsComponent,
+    TestimoniesComponent,
+    PrayerComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
