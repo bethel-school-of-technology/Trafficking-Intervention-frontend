f3b4f8e0cb8d38d03f0ade547d6225dffcc0e95f
Chelsie Lancaster <chelsandgrace@gmail.com>
2019-09-02 14:12:34 -0500

    figuring out http post request

--- src/app/components/prayer/prayer.component.ts
+++ src/app/components/prayer/prayer.component.ts
@@ -1,28 +1,63 @@
 import { Component, OnInit } from '@angular/core';
 import { PrayerService } from '../../services/prayer.service';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Prayer } from '../../models/prayer';
-
-
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-prayer',
   templateUrl: './prayer.component.html',
   styleUrls: ['./prayer.component.css']
 })
-export class PrayerComponent implements OnInit {
-formData: Prayer;
-  constructor(private prayerService: PrayerService) { }
+export class PrayerComponent {
+
+// formData: Prayer;
+postURL: 'http://localhost:5000/api/PrayerRequest';
+
+
+  constructor(private httpClient: HttpClient) { }
+  
+  // fetch(postURL)
+  // .then(res => res.blob()) // Gets the response and returns it as a blob
+  // .then(blob => {
+  //   var formData = new FormData();
+  //   formData.append("file", blob);
+
+  //   this.http.post(prayerRequest, formData).subscribe(response => {
+  //       console.log(response);
+  //   });
+
+  // });
+  // from: https://stackoverflow.com/questions/57628638/angular-send-remote-file-with-angular-http-form-data
+
+  // postPrayerRequest(): void {
+  //   const newPrayerRequest: PrayerRequest = Object.assign({}, this.prayerRequest);
+  //   this._prayerRequestService.post(this.prayerRequest).subscribe(
+  //     (data: PrayerRequest) => {
+  //       console.log(data);
+  //       this.createPrayerRequestForm.reset();
+  //       this._router.navigate(['list']);
+  //     },
+  //     (error: any) => console.log(error)
+  //     );
+  // from: https://www.youtube.com/watch?v=B1LRD7oMYDo
+ 
+  //   this.http.post<Prayer[]>(this.postURL).subscribe(prayerRequest=>{
+  //     this.prayerRequest = prayerRequest;
+  //   })
+  
+  // ** from exeter front end foundations http lesson page 7
   // this.prayer
     // this.prayerService.getPrayerRequest().subscribe((res)=>{
     //   this.prayerService.getPrayerRequest(this.prayerService).subscribe((res)=>{
     //     console.log(res.body);
     //   });      
     // });
-    ngOnInit(){
+    // ngOnInit(){
       // this.prayerService.getPrayerRequest().subscribe((res)=>{
       //   this.prayerService.getPrayerRequest(this.prayerService).subscribe((res)=>{
       //     console.log(res.body);
         // });      
-      }}
-      // <fetch-request></fetch-request>
+      // }}
+}
--- src/app/services/prayer.service.ts
+++ src/app/services/prayer.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpRequest, HttpEvent, HttpEventType } from '@angular/common/http';
+import { HttpClient, HttpRequest, HttpEvent, HttpEventType, HttpHeaders } from '@angular/common/http';
 import { Prayer } from '../models/prayer';
+import { Observable } from 'rxjs';
+import { PrayerComponent } from '../components/prayer/prayer.component';
 
 export interface PrayerRequest {
   id: number;
@@ -16,10 +18,45 @@ export interface PrayerRequest {
 })
 
 export class PrayerService {
-  apiURL: string ='http://localhost:5000/api/PrayerRequest';
-  postURL: string ='http://localhost:5000/api/PrayerRequest';
+apiURL: string ='http://localhost:5000/api/PrayerRequest';
+postURL: string ='http://localhost:5000/api/PrayerRequest';
+  // httpClient: any;
+
+// // @Injectable({
+//   providedIn: 'root'
+// })
+// export class ShippingService {
+
+//   private shippingURL : string = 'api/Shipping/shippingDoc';
+//   constructor(private http: HttpClient) { }
+
+//   postShippingDocForm(shippingDoc : ShippingDoc) : Observable<any> {
+//     var headers = new HttpHeaders().set('Content-Type', 'application/json; charset=utf-8');
+//     var test = {name: "hello"} //dummy data
+//     return this.http.post(this.shippingURL, test, {headers: headers});
+//   }
+// }
 
   constructor(private http: HttpClient) { }
+  postPrayerequest(prayerRequest : PrayerRequest) : Observable<any> {
+        var headers = new HttpHeaders().set('Content-Type', 'application/json; charset=utf-8');
+        var test = {name: "hello"} //dummy data
+        return this.http.post(this.postURL, test, {headers: headers});
+      }
+
+  // callServer(data: {content: string}): Observable<any> {
+  //   return this.httpClient.post(this.postURL, data);
+  // }
+
+  // private listPrayerRequest: PrayerRequest[] = [
+
+  // ];
+
+  // postPrayerRequest(){
+  //   this.http.post<PrayerRequest[]>(this.postURL).subscribe(prayerRequest=>{
+  //     this.prayerRequest = prayerRequest;
+  //   })
+  // }
   // router.post(apiURL, function (req, res, next) {
   //   models.prayer.create(req.body)
   //     .then(newPrayerRequest => {
@@ -32,21 +69,36 @@ export class PrayerService {
   //     });
   // });
 
-  public getPrayerRequest(){
-    return this.http.get<Prayer[]>(`${this.apiURL}`);
-}
 
-public createPrayerRequest(prayerRequest: Prayer){
-    return this.http.post(`${this.postURL}`, prayerRequest);
-}
+// post(prayerRequest: PrayerRequest): Observable<PrayerRequest>{
+//   if (prayerRequest.id === null){
+//     return this.httpClient.post<PrayerRequest>(this.postURL, prayerRequest, {
+//       headers: new HttpHeaders ({
+//         'Content-Type': 'application/json'
+//       })
+//     })
+//     .pipe (catchError(this.handleError));
+//     this.listPrayerRequest.push(prayerRequest);
+//   } else {
+//     const foundIndex = this.listPrayerRequest.findIndex (e => e.id === prayerRequest.id);
+//     this.listPrayerRequest[foundIndex] = prayerRequest;
+//   }
+// }
+// from: https://www.youtube.com/watch?v=B1LRD7oMYDo
 
-public updatePrayerRequest(prayerRequest: Prayer){
-  return this.http.put(`${this.apiURL}`, prayerRequest);
-}
+// createPrayerRequest(prayerRequest: Prayer): Observable<Prayer>{
+//     return this.http.post<Prayer>(`${this.postURL}`, prayerRequest);
+// }
 
-public deletePrayerRequest(prayerRequest: Prayer){
-  return this.http.put(`${this.apiURL}`, prayerRequest);
-}
+
+
+// public updatePrayerRequest(prayerRequest: Prayer){
+//   return this.http.put(`${this.apiURL}`, prayerRequest);
+// }
+
+// public deletePrayerRequest(prayerRequest: Prayer){
+//   return this.http.put(`${this.apiURL}`, prayerRequest);
+// }
 
 }
 
