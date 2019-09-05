021afeb57206c11ef534d5cbc050bf80bb120431
Chelsie Lancaster <chelsandgrace@gmail.com>
2019-09-04 22:48:45 -0500

    Changed prayer component and services for post

--- src/app/services/prayer.service.ts
+++ src/app/services/prayer.service.ts
@@ -1,139 +1,30 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpRequest, HttpEvent, HttpEventType, HttpHeaders } from '@angular/common/http';
-import { Prayer } from '../models/prayer';
-import { Observable } from 'rxjs';
-import { PrayerComponent } from '../components/prayer/prayer.component';
-
-export interface PrayerRequest {
-  id: number;
-  firstName: string;
-  lastName: string;
-  prayer: string;
-  date: Date;
-  site: string;
-}
-
-// getPrayer(){
-//   return this.http.get<Prayer>(this.postURL)
-// };
+import { HttpClient } from '@angular/common/http';
+// import { Prayer } from '../models/prayer';
+// import { Observable } from 'rxjs';
+// import { PrayerComponent } from '../components/prayer/prayer.component';
 
 @Injectable({ 
   providedIn: 'root'
 })
 
-
 export class PrayerService {
+  items = [];
+  
+  
+  constructor(private http: HttpClient){}
 
-postURL: 'http://localhost:5000/api/PrayerRequest';
+  addToPrayer(prayer) {
+    this.items.push(this.items);
+  }
 
-getPrayer() {
-//   fetch(this.postURL,{
-//     method: 'POST',
-//     body: JSON.stringify(document.getElementById('postData')),
-//     mode: 'cors', // no-cors, cors, *same-origin
-//     cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-//     credentials: 'omit',
-//     headers: {
-//       'Content-Type': 'application/json'
-//     }
-//   })
-//   .then(res => res.json())
-// .then(response => console.log('Success:', JSON.stringify(response)))
-// .catch(error => console.error('Error:', error));
+  getItems() {
+    return this.items;
+  }
 
-}
-
-// apiURL: string ='http://localhost:5000/api/PrayerRequest';
-// postURL: string ='http://localhost:5000/api/PrayerRequest';
-
-constructor(private http: HttpClient){}
-// getPrayer(){
-//   return this.http.get<Prayer>(this.postURL)
-// };
-
-// prayer: Prayer;
-// showPrayer(){
-//   this.
-// }
-  // httpClient: any;
-
-// // @Injectable({
-//   providedIn: 'root'
-// })
-// export class ShippingService {
-
-//   private shippingURL : string = 'api/Shipping/shippingDoc';
-//   constructor(private http: HttpClient) { }
-
-//   postShippingDocForm(shippingDoc : ShippingDoc) : Observable<any> {
-//     var headers = new HttpHeaders().set('Content-Type', 'application/json; charset=utf-8');
-//     var test = {name: "hello"} //dummy data
-//     return this.http.post(this.shippingURL, test, {headers: headers});
-//   }
-// }
-
-  // constructor(private http: HttpClient) { }
-  // postPrayerequest(prayerRequest : PrayerRequest) : Observable<any> {
-  //       var headers = new HttpHeaders().set('Content-Type', 'application/json; charset=utf-8');
-  //       var test = {name: "hello"} //dummy data
-  //       return this.http.post(this.postURL, test, {headers: headers});
-  //     }
-
-  // callServer(data: {content: string}): Observable<any> {
-  //   return this.httpClient.post(this.postURL, data);
-  // }
-
-  // private listPrayerRequest: PrayerRequest[] = [
-
-  // ];
-
-  // postPrayerRequest(){
-  //   this.http.post<PrayerRequest[]>(this.postURL).subscribe(prayerRequest=>{
-  //     this.prayerRequest = prayerRequest;
-  //   })
-  // }
-  // router.post(apiURL, function (req, res, next) {
-  //   models.prayer.create(req.body)
-  //     .then(newPrayerRequest => {
-  //       res.setHeader('Content-Type', 'application/json');
-  //       res.send(JSON.stringify(newPrayerRequest));
-  //     })
-  //     .catch(err => {
-  //       res.status(400);
-  //       res.send(err.message);
-  //     });
-  // });
-
-
-// post(prayerRequest: PrayerRequest): Observable<PrayerRequest>{
-//   if (prayerRequest.id === null){
-//     return this.http.post<PrayerRequest>(this.postURL, prayerRequest, {
-//       headers: new HttpHeaders ({
-//         'Content-Type': 'application/json'
-//       })
-//     })
-//     .pipe (catchError(this.handleError));
-//     this.listPrayerRequest.push(prayerRequest);
-//   } else {
-//     const foundIndex = this.listPrayerRequest.findIndex (e => e.id === prayerRequest.id);
-//     this.listPrayerRequest[foundIndex] = prayerRequest;
-//   }
-// }
-// from: https://www.youtube.com/watch?v=B1LRD7oMYDo
-
-// createPrayerRequest(prayerRequest: Prayer): Observable<Prayer>{
-//     return this.http.post<Prayer>(`${this.postURL}`, prayerRequest);
-// }
-
-
-
-// public updatePrayerRequest(prayerRequest: Prayer){
-//   return this.http.put(`${this.apiURL}`, prayerRequest);
-// }
-
-// public deletePrayerRequest(prayerRequest: Prayer){
-//   return this.http.put(`${this.apiURL}`, prayerRequest);
-// }
-
+  clearItems() {
+    this.items = [];
+    return this.items;
+  }
 }
 
