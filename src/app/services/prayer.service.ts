70f8d40a6fc78606501344b34881564f629fa7d9
Justin Shannon <42521970+papa7wheelie@users.noreply.github.com>
2019-09-10 18:14:28 -0400

    Modification

--- src/app/services/prayer.service.ts
+++ src/app/services/prayer.service.ts
@@ -1,28 +1,32 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-@Injectable({ 
+@Injectable({
   providedIn: 'root'
 })
 
 export class PrayerService {
   items = [];
   postURL: 'http://localhost:5000/api/PrayerRequest';
-  
-  
-  constructor(private http: HttpClient){}
 
-  addToPrayer(items) {
-    this.items.push(this.postURL, this.items);
+constructor(private http: HttpClient){}
+
+
+  postPrayer(items: any) {
+    this.items.push(this.postURL,this.items)
   }
-
   getItems() {
     return this.items;
   }
-
-  clearItems() {
-    this.items = [];
-    return this.items;
+  putItems() {
+    return this.putItems;
   }
+  patchItems() {
+    return this.patchItems;
+  }
+  deleteItems() {
+    return this.deleteItems;
+  }
+
 }
 
