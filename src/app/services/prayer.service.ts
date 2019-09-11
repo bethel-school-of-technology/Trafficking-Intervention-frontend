bbfb9e3907203190dc7471289cc00befa1f234a0
Chelsie Lancaster <chelsandgrace@gmail.com>
2019-09-11 10:00:49 -0500

    changes

--- src/app/services/prayer.service.ts
+++ src/app/services/prayer.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpRequest, HttpEvent, HttpEventType } from '@angular/common/http';
+import { Prayer } from '../models/prayer';
+import { Observable } from 'rxjs';
+// import { PrayerComponent } from '../components/prayer/prayer.component';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({ 
   providedIn: 'root'
@@ -8,10 +12,11 @@ import { HttpClient } from '@angular/common/http';
 export class PrayerService {
   items = [];
   postURL: 'http://localhost:5000/api/PrayerRequest';
+
   
   
   constructor(private http: HttpClient){}
-
+ 
   addToPrayer(items) {
     this.items.push(this.postURL, this.items);
   }
