8c6de324b878132212f7dfddf78e068897657aba
Tim Bishop <Timbishop2018@gmail.com>
2019-09-11 23:05:22 -0400

    post to db

--- src/app/components/prayer/prayer.component.ts
+++ src/app/components/prayer/prayer.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ɵConsole } from '@angular/core';
 import { PrayerService } from '../../services/prayer.service';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpClient, HttpRequest, HttpHeaders, } from '@angular/common/http';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { post } from 'selenium-webdriver/http';
-
-
+import { post} from 'selenium-webdriver/http';
+import { throwError, Observable } from 'rxjs';
+import { tap, catchError, map} from 'rxjs/operators';
+import {Prayer } from '../../models/prayer';
 
 @Component({
   selector: 'app-prayer',
@@ -16,12 +17,18 @@ import { post } from 'selenium-webdriver/http';
 export class PrayerComponent {
   items;
   prayerForm: FormGroup;
-  postURL: 'http://localhost:5000/api/PrayerRequest';
+  postURL: 'http://localhost:5000/api/PrayerRequest/';
+  headers = new HttpHeaders().set('Content-Type', 'application/json').set('Accept', 'application/json');
+  httpOptions = {
+    headers: this.headers
+  };
+  //constructor(private httpClient:HttpClient){}
 
 
   constructor(
     private prayerService: PrayerService,
     private formBuilder: FormBuilder,
+    private httpClient: HttpClient
     ) {
       this.items = this.prayerService.getItems();
       this.prayerForm = this.formBuilder.group({
@@ -32,37 +39,68 @@ export class PrayerComponent {
       });
      }
 
-     onSubmit(prayerData) {
+     handlePost(prayerData: Prayer): Observable<Prayer>{
+      return this.httpClient.post<Prayer>('http://localhost:5000/api/PrayerRequest/', prayerData, this.httpOptions).pipe(
+        tap(data => console.log(data)),
+        catchError(this.handleError)
+      );
+     }
+     private handleError(error: any) {
+      console.log(error);
+      return throwError(error);
+    }
 
-    
+     onSubmit(prayerData) {
+      this.handlePost(prayerData).subscribe(data => console.log(data))
+      console.log("hello");
       console.warn('Your prayer request has been submitted', prayerData);
   
-      this.items = this.prayerService.clearItems();
-      this.prayerForm.reset();
-    }
-    getPrayer() {
+      //this.items = this.prayerService.clearItems();
+      //this.prayerForm.reset();
 
-      function postData(postURL, Data = {}) {
-  fetch(this.postURL,{
-    method: 'POST',
-    body: JSON.stringify(this.items),
-    mode: 'cors', // no-cors, cors, *same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'omit',
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  })
-
-  .then(res => res.json())
-
-.then(response => this.http.post(postURL, JSON.stringify(response)))
-.catch(error => console.error('Error:', error));
-
-    }
-  }
+      }
+      // public int AppUserID {get; set; }
+      // public string FirstName {get; set; }
+      // public string LastName {get; set; }
+      // public string PrayerRequest {get; set; }    
+        
+      // public string Date {get; set; }
+      
+      // public string Sites {get; set; }
+        postRequest(){
+          this.httpClient.post('http://localhost:5000/api/PrayerRequest/', JSON.stringify(this.prayerForm)
+            // {
+            //   method: 'POST',
+            //   body: JSON.stringify(this.prayerForm),
+            //   mode: 'cors', // no-cors, cors, *same-origin
+            //   cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            //   credentials: 'omit',
+            //   headers: {
+            //     'Content-Type': 'application/json'
+            //   }
+            //}
+          )
+              
+          .subscribe(
+            // {
+            //   method: 'POST',
+            //   body: JSON.stringify(this.prayerForm),
+            //   mode: 'cors', // no-cors, cors, *same-origin
+            //   cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            //   credentials: 'omit',
+            //   headers: {
+            //     'Content-Type': 'application/json'
+            // }
+          )
+        }
+           
+    //       //.then(res => res.json())
+    //       // .then(response => console.log('Success:', JSON.stringify(response)))
+    //       // .catch(error => console.error('Error:', error));
+    // }
   
-  ngOnInit()    {  }
+ ngOnInit(){}
 
 }
+
 
--- src/app/services/prayer.service.ts
+++ src/app/services/prayer.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { getElementDepthCount } from '@angular/core/src/render3/state';
 // import { Prayer } from '../models/prayer';
 // import { Observable } from 'rxjs';
 // import { PrayerComponent } from '../components/prayer/prayer.component';
@@ -11,12 +12,18 @@ import { HttpClient } from '@angular/common/http';
 export class PrayerService {
   items = [];
   
-  
   constructor(private http: HttpClient){}
 
   addToPrayer(items) {
-    this.items.push(this.items);
+    this.items.push(items);
   }
+
+  // addPrayer (prayer: Prayer): Observable<Prayer> {
+  //   return this.http.post<Prayer>(this.postURL, prayer, httpOptions)
+  //     .pipe(
+  //       catchError(this.handleError('addPrayer', hero))
+  //     );
+  // }
 
   getItems() {
     return this.items;
